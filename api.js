1081922c883a72209fa86cdfd9cbb69ee00f9d97
Joseph Chiang <jccofresh@gmail.com>
2019-03-02 20:59:32 +0000

    api quote generator

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 const request = new XMLHttpRequest();
-const url = "https://api.adviceslip.com/advice";
+const url = "https://quotes.stormconsultancy.co.uk/quotes/1.json?callback=my_method";
 let advice = document.getElementById("advice");
 function xmlOpen() {
 request.open("GET", url, true);
@@ -7,7 +7,7 @@ request.onload = function () {
         let data = JSON.parse(this.response); //parses the response JSON string into javascript object, store this parsed data in variable called data
         if (request.status >= 200 && request.status < 400) {
             //condition for when request is succesful
-            advice.innerHTML = data.slip.advice;
+            advice.innerHTML = data.quote;
         }
         else {
             advice.innerHTML = "Advice not found!";
